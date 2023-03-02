4f7cbf6aec4a612a60b6ea092819099d0b249a40
= <=>
2023-03-02 16:55:57 -0500

    creacion proyecto back con dependencias iniciales

---
+++ Back/back_empresarial/src/main/java/co/edu/unicauca/core/back_empresarial/BackEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.core.back_empresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEmpresarialApplication.class, args);
+	}
+
+}
